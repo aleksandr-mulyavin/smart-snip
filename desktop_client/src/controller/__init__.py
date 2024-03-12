8eb1e8b934efb2b76fff4bda59440dabece5bddb
MulyavinAA <mulyavin_aa@hotmail.com>
2024-03-12 21:05:39 +0500

    Добавлен класс конфига для десктопного приложения

--- desktop_client/src/controller/__init__.py
+++ desktop_client/src/controller/__init__.py
@@ -1,2 +1,1 @@
 from .image_viewer import open_stand_image_viewer
-from .config_reader import ConfigReader
--- desktop_client/src/utils/__init__.py
+++ desktop_client/src/utils/__init__.py
@@ -1,1 +1,2 @@
 from .sys_event_key import QtKeyBinder
+from .config_reader import Config, ConfigReader
