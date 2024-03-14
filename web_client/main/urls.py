85fc8b83c16c2a92416ca8e3e4c00d6d9a5dbc14
Ekaterina Zvereva <evzvereva1@gmail.com>
2024-03-14 23:46:11 +0500

    Fix url

--- web_client/main/urls.py
+++ web_client/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home'),
+    path('app', views.app, name='app'),
     path('about', views.about, name='about')
 ]
