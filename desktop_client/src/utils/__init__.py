71feff8aa2f4190895b06aa1f7eee824103eca3d
MulyavinAA <mulyavin_aa@hotmail.com>
2024-03-16 16:39:43 +0500

    Добавлен импорт фунции распознавания текста на изображении

--- desktop_client/src/utils/__init__.py
+++ desktop_client/src/utils/__init__.py
@@ -1,3 +1,4 @@
 from .sys_event_key import QtKeyBinder
 from .image_viewer import open_stand_image_viewer, Image
 from .config_reader import Config, ConfigReader
+from .api_caller import call_image_to_text
