549f2aee38c8561bddbaad3bf2559cc0ee11e9a5
MulyavinAA <mulyavin_aa@hotmail.com>
2024-03-21 23:29:12 +0500

    Добавлен вызов метода image_to_data

--- desktop_client/src/utils/api_caller.py
+++ desktop_client/src/utils/api_caller.py
@@ -5,6 +5,7 @@ from io import BytesIO
 from PIL.Image import Image
 
 from ..api_models import Error, ImageToTextRequest
+from ..api_models import ImageToDataResponse
 
 API_METHOD = 'image_to_text'
 
@@ -26,5 +27,26 @@ def call_image_to_text(url: str, token: str, image: Image) -> str:
         print(http_response, http_response.text)
         if http_response.status_code == requests.codes.ok:
             return http_response.text
+    except Exception as e:
+        print(e)
+
+
+def call_image_to_data(url: str, token: str, image: Image) -> ImageToDataResponse:
+    print(url, token)
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+
+    headers = {'X-API-key': token,
+               'Content-Type': 'application/json'}
+
+    request = ImageToTextRequest(
+        image=img_str)
+
+    try:
+        http_response = requests.post(f'{url}{API_METHOD}', headers=headers, json=request.model_dump(mode='json'))
+        print(http_response, http_response.text)
+        if http_response.status_code == requests.codes.ok:
+            return ImageToDataResponse(**http_response.json())
     except Exception as e:
         print(e)
