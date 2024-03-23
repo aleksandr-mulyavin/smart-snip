055f95135f9a97ba1714444c6bf42e67ac1244b2
Ekaterina Zvereva <evzvereva1@gmail.com>
2024-03-24 00:10:08 +0500

    Add function handle_uploaded_file and button file_upload

--- web_client/main/views.py
+++ web_client/main/views.py
@@ -6,25 +6,35 @@ def home(request):
     return render(request, 'main/home.html')
 
 
+def handle_uploaded_file(f):
+    """
+    Позволяет сохранить файл, который загрузил пользователь
+    """
+    with open(f"main/temp_files/{f.name}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def app(request):
-    s = t() # временная функция с текстом
-    result_translate = get_to_text_translate(s, 'ru') # функция с переводом
+    s = t()  # временная функция с текстом
+    result_translate = get_to_text_translate(s, 'ru')  # функция с переводом
     if request.method == "POST":
-        user_id = request.POST.get("lang_elements")
-        output = user_id
-        result_translate = get_to_text_translate(s, output) # функция с переводом
-        language_and_code = DictLanguage()
+        lang_elements = request.POST.get("lang_elements")
+        output = lang_elements
+        result_translate = get_to_text_translate(s, output)  # функция с переводом
+        language_and_code = DictLanguage()  # форма с ниспадающим списком
+        handle_uploaded_file(request.FILES["file_upload"])  # получение загруженного файла пользователем
         return render(request, 'main/app.html', {"language_and_code": language_and_code,
-                                                                        "result_translate": result_translate,
-                                                                        "t": s,
-                                                                        "iso_639_1_languages": iso_639_1_languages})
+                                                 "result_translate": result_translate,
+                                                 "t": s,
+                                                 "iso_639_1_languages": iso_639_1_languages})
 
     else:
         language_and_code = DictLanguage()
     return render(request, 'main/app.html', {"language_and_code": language_and_code,
-                                                                    "result_translate": result_translate,
-                                                                    "t": s,
-                                                                    "iso_639_1_languages": iso_639_1_languages})
+                                             "result_translate": result_translate,
+                                             "t": s,
+                                             "iso_639_1_languages": iso_639_1_languages})
 
 
 def about(request):
