f873eb028d944c6e91115cab3f22606ce568caf8
Ekaterina Zvereva <evzvereva1@gmail.com>
2024-03-24 01:13:40 +0500

    Add new forms.py, add class "UploadFileForm" and transfer class DictLanguage from translate.py

---
+++ web_client/main/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .translate_text import iso_639_1_languages
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label="")
+
+
+class DictLanguage(forms.Form):
+    """
+    Создает форму для выбора из выпадающего списка названия языков
+    """
+    lang_elements = forms.ChoiceField(choices=iso_639_1_languages, label='')
