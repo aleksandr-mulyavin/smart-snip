e318d5d05239b7a3d5ea842db87d643528aff2fa
Ekaterina Zvereva <evzvereva1@gmail.com>
2024-03-24 20:05:21 +0500

    Add a static path to the uploaded image by the user in handle_uploaded_file and add file_path_static in render

--- web_client/main/views.py
+++ web_client/main/views.py
@@ -11,9 +11,14 @@ def handle_uploaded_file(f):
     """
     Позволяет сохранить файл, который загрузил пользователь
     """
-    with open(f"main/temp_files/{f.name}", "wb+") as destination:
+    file_path = f"main/static/main/image/{f.name}"
+    with open(file_path, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+    if file_path:
+        return file_path[12:]  # Получаем путь к static, куда сохранился файл для отображения на веб-сайте
+    else:
+        return None
 
 
 def app(request):
@@ -21,13 +26,14 @@ def app(request):
     result_translate = get_to_text_translate(s, 'ru')  # функция с переводом
     language_and_code = DictLanguage()  # форма с ниспадающим списком
     upload_file = UploadFileForm()
-
+    file_path_static = None
     if request.method == "POST":
         form_type = request.POST.get("form_type")
         if form_type == 'upload_file':
             upload_file = UploadFileForm(request.POST, request.FILES)
             if upload_file.is_valid():
-                handle_uploaded_file(upload_file.cleaned_data['file'])  # получение загруженного файла пользователем
+                file_path_static = handle_uploaded_file(upload_file.cleaned_data['file'])  # получение загруженного файла пользователем
+
         elif form_type == 'change_language':
             lang_elements = request.POST.get("lang_elements")
             output = lang_elements
@@ -37,7 +43,7 @@ def app(request):
                                              "result_translate": result_translate,
                                              "t": s,
                                              "iso_639_1_languages": iso_639_1_languages,
-                                             "upload_file": upload_file})
+                                             "upload_file": upload_file, "file_path_static": file_path_static})
 
 
 def about(request):
