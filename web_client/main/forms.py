c2dce986321bcf00bff3cf7875f76362a3248a16
KuznetsovAV <kav.logrus@gmail.com>
2024-03-26 11:48:37 +0400

    fix translate_image_text method in API, change forms, html and css in web client

--- web_client/main/forms.py
+++ web_client/main/forms.py
@@ -6,8 +6,10 @@ class UploadFileForm(forms.Form):
     file = forms.ImageField(label="")
 
 
-class DictLanguage(forms.Form):
+class TranslatorForm(forms.Form):
     """
     Создает форму для выбора из выпадающего списка названия языков
     """
+    source_text = forms.CharField(widget=forms.Textarea)
     lang_elements = forms.ChoiceField(choices=iso_639_1_languages, label='')
+    translated_text = forms.CharField(widget=forms.Textarea)
