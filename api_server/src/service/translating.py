d3ddf9f5325effcf0691f8b9aa2d73b4874f7aa1
Ivan <ivanivanovplayer2019@gmail.com>
2024-03-26 12:45:05 +0100

    Add def translate(), class Translator

--- api_server/src/service/translating.py
+++ api_server/src/service/translating.py
@@ -1,9 +1,14 @@
+from translate import Translator as ExtTranslator
 
 
 class Translator():
-    def __init__(self, from_lang, to_lang):
+    def __init__(self, to_lang, from_lang='en'):
         self.from_lang = from_lang
         self.to_lang = to_lang
 
     def translate(self, text: str) -> str:
-        return text
+        translator = ExtTranslator(
+            to_lang=self.to_lang,
+            from_lang=self.from_lang,
+        )
+        return translator.translate(text)
