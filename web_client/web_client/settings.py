42b4a505dc59bc949278363c1a3c5bc14b4681b9
KuznetsovAV <kav.logrus@gmail.com>
2024-04-01 13:16:11 +0400

    fix debug mode in settings.py

--- web_client/web_client/settings.py
+++ web_client/web_client/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-if os.getenv("DJANGO_DEBUG") == 1:
+if os.getenv("DJANGO_DEBUG") == "1":
     DEBUG = True
 
 ALLOWED_HOSTS = os.getenv(
