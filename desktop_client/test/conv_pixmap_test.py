60cc18dcc215c2af5f0f9d042766eac1b5573cff
MulyavinAA <mulyavin_aa@hotmail.com>
2024-04-08 15:06:16 +0500

    Fix тестов модуля image_viewer.py десктопного приложения

--- desktop_client/test/conv_pixmap_test.py
+++ desktop_client/test/conv_pixmap_test.py
@@ -1,20 +1,22 @@
 import PIL.Image
 
-from pathlib import Path
-
-from . import get_module_from_file, get_func_from_module
+from . import (get_module_from_file, get_func_from_module,
+               get_class_from_module)
 
 from PIL.Image import Image
 from PyQt5.QtGui import QPixmap
 
-resource_py = get_module_from_file('image_viewer.py')
-conv_to_pixmap_func = get_func_from_module('conv_to_pixmap', resource_py)
+image_viewer_py = get_module_from_file('image_viewer.py')
+conv_to_pixmap_func = get_func_from_module('conv_to_pixmap', image_viewer_py)
+
+resource_py = get_module_from_file('resource.py')
+resource_finder_class = get_class_from_module('ResourceFinder', resource_py)
 
 
-def _test_conv_pixmap():
-    path = Path(__file__).parent.joinpath(
-        'resources/snip_29_03_2024_21_05_13.png')
-    image: Image = PIL.Image.open(path)
+def test_conv_pixmap():
+    path_png = resource_finder_class().find_resource_file(
+        'snip_29_03_2024_21_05_13.png')
+    image: Image = PIL.Image.open(path_png)
     pixmap: QPixmap = conv_to_pixmap_func(image)
     assert pixmap is not None
     assert isinstance(pixmap, QPixmap)
