ed799bc4e6ba04ff15ba5efcab250919f260c57e
MulyavinAA <mulyavin_aa@hotmail.com>
2024-04-08 20:28:10 +0500

    Fix теста conv_pixmap_test.py

--- desktop_client/test/conv_pixmap_test.py
+++ desktop_client/test/conv_pixmap_test.py
@@ -1,23 +1,17 @@
-import PIL.Image
-
-from . import (get_module_from_file, get_func_from_module,
-               get_class_from_module)
+import PIL
 
 from PIL.Image import Image
 from PyQt5.QtGui import QPixmap
 
-image_viewer_py = get_module_from_file('image_viewer.py')
-conv_to_pixmap_func = get_func_from_module('conv_to_pixmap', image_viewer_py)
-
-resource_py = get_module_from_file('resource.py')
-resource_finder_class = get_class_from_module('ResourceFinder', resource_py)
+from ..src.utils.resource import ResourceFinder
+from ..src.utils.image_viewer import conv_to_pixmap
 
 
 def test_conv_pixmap():
-    path_png = resource_finder_class().find_resource_file(
+    path_png = ResourceFinder().find_resource_file(
         'snip_29_03_2024_21_05_13.png')
     image: Image = PIL.Image.open(path_png)
-    pixmap: QPixmap = conv_to_pixmap_func(image)
+    pixmap: QPixmap = conv_to_pixmap(image)
     assert pixmap is not None
     assert isinstance(pixmap, QPixmap)
     assert pixmap.width() == image.width
